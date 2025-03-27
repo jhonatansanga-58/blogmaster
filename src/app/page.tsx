82ee3585c9961a59d0a932f78a1e4f4315577b3d
jhonatansanga58 <sbj2019949@est.univalle.edu>
2025-03-27 01:11:44 -0400

    feat: first version of the feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+export default async function Home() {
+    const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' });
+    const posts = await res.json();
+
     return (
-        <div className='flex items-center justify-center min-h-screen bg-gray-200'>
-            <h1 className='text-4xl font-bold text-blue-600'>HELLO</h1>
+        <div className='container mx-auto p-4'>
+            <h1 className='text-3xl font-bold'>Blogs</h1>
+
+            {posts.map((post) => (
+                <div
+                    key={post.id}
+                    className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'
+                >
+                    <div className='p-4'>
+                        <h2 className='text-xl font-bold'>{post.title}</h2>
+                        <p className='text-gray-500'>{post.content.toString().slice(0, 30)}...</p>
+                        <a href={'/blog/${post.id}'} className='text-blue-600 hover:underline'>
+                            Leer más
+                        </a>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
