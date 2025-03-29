adff5606c6c2d32c5b976e93e3f0d5bbd9d828ea
jhonatansanga58 <sbj2019949@est.univalle.edu>
2025-03-28 20:11:52 -0400

    feat: create the post component and the main feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import PostCard from '../../components/post-card';
+
 export default async function Home() {
     const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' });
     const posts = await res.json();
@@ -5,20 +7,8 @@ export default async function Home() {
     return (
         <div className='container mx-auto p-4'>
             <h1 className='text-3xl font-bold'>Blogs</h1>
-
             {posts.map((post) => (
-                <div
-                    key={post.id}
-                    className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'
-                >
-                    <div className='p-4'>
-                        <h2 className='text-xl font-bold'>{post.title}</h2>
-                        <p className='text-gray-500'>{post.content.toString().slice(0, 30)}...</p>
-                        <a href={'/blog/${post.id}'} className='text-blue-600 hover:underline'>
-                            Leer más
-                        </a>
-                    </div>
-                </div>
+                <PostCard key={post.id} post={post} />
             ))}
         </div>
     );
