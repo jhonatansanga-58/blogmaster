22c3b46a81f238656ad811a908786f10c68de518
jhonatansanga58 <sbj2019949@est.univalle.edu>
2025-03-29 02:47:58 -0400

    feat: request and show post

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function Blog() {
+export default async function Blog({ params }: { params: { id: string } }) {
+    const res = await fetch(`http://localhost:3000/api/posts/${params.id}`);
+
+    if (!res.ok) {
+        return (
+            <div className='container mx-auto max-w-3xl mt-6'>
+                <h1 className='text-3xl font-bold'>We can&apos;t find this post!</h1>
+            </div>
+        );
+    }
+
+    const post = await res.json();
+
     return (
-        <div className='flex items-center justify-center min-h-screen bg-gray-200'>
-            <h1 className='text-4xl font-bold text-blue-600'>A Blog</h1>
+        <div className='container mx-auto max-w-3xl mt-6 p-6 bg-white border border-gray-200 rounded-lg shadow-sm'>
+            <h1 className='text-3xl font-bold'>{post.title}</h1>
+            <p className='text-gray-700 mt-4'>{post.content}</p>
         </div>
     );
 }
--- src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Blog() {
-    return (
-        <div className='flex items-center justify-center min-h-screen bg-gray-200'>
-            <h1 className='text-4xl font-bold text-blue-600'>A Blog</h1>
-        </div>
-    );
-}
